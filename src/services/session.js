0dfb552f98fe8b7806ebcaed5c6dfa9eb1633ab9
Valentin Cassarino <valencassa@gmail.com>
2022-11-11 16:51:03 -0300

    Fixed token handler

--- src/services/session.js
+++ src/services/session.js
@@ -2,11 +2,14 @@ import jwt from 'jsonwebtoken';
 import { env } from '$env/dynamic/private';
 
 export const getSession = async (token) => {
-	const decodedToken = token ? jwt.verify(token, env.JWT_SECRET) : false;
-
-	if (!token || !decodedToken.password) {
+	try {
+		const decodedToken = token ? jwt.verify(token, env.JWT_SECRET) : false;
+		if (!token || !decodedToken.password) {
+			return false;
+		} else {
+			return true;
+		}
+	} catch (e) {
 		return false;
-	} else {
-		return true;
 	}
 };
